3fac805479deab91d1bd3b5acc52ccbb5daff678
Naomi Liu <nhliu@ucdavis.edu>
2020-11-12 19:02:52 +0000

    Finished tests

---
+++ model.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Board game."""
+
+    __tablename__ = "games"
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(100))
+
+
+def connect_to_db(app, db_uri="postgresql:///gamesdb"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
+
+
+def example_data():
+    """Create example data for the test database."""
+    # FIXME: write a function that creates a game and adds it to the database.
+    Game.query.delete()
+    game = Game(name="chess", description="board game")
+    game1 = Game(name="uno", description="card game")
+    db.session.add_all([game,game1])
+    db.session.commit()
+
+    # print("FIXME")
+
+
+if __name__ == '__main__':
+    from party import app
+
+    connect_to_db(app)
+    print("Connected to DB.")
